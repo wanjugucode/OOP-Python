0f9eec42134a4aecd8d11eeeb004c05a0c0ec293
wanjugucode <karobiaanastasia.com>
2021-05-24 20:57:34 +0300

    code

--- bank.py
+++ bank.py
@@ -1,9 +1,42 @@
-class BankAccount:
-    name="consolidated bank"
-    def __init__(self,banking,withdraw):
-        self.banking=banking
-        self.withdraw=withdraw
-    def newest_account(self):
-        return f"I will deposit sh.{self.banking} today"
-    def parent_account(self):
-        return f"I will withdraw sh{self.withdraw} to pay my employee"
+
+class BankAcc:
+    name="Equity"
+    def __init__(self,name,phone_number):
+        self.name=name
+        self.phone_number=phone_number
+        self.balance=0
+        
+    def show_balance(self):
+        return f"Hello{self.name}you balance is{self.balance}"
+    def deposit(self,amount):
+        self.balance+=amount
+        return self.show_balance()
+
+    def withdraw(self,amount):
+        if amount>self.balance:
+            return f"you can't withdraw {amount}it is below minimum"
+          
+        else:
+             self.balance-=amount
+             return self.show_balance()
+           
+       
+    def update_deposit(self,amount):
+        if amount>0:  
+            self.balance+=amount
+            return f"Hello your new balance is {self.show_balance}" 
+        else:
+            return f"you cannot deposit{amount} it is beyound minimum"
+       
+    def borrow(self,amount):
+        self.loan=0
+        self.loan+=amount
+        return f"Hello your loan balance is{self.loan}" 
+
+    def repay(self,amount):
+        self.loan-=amount
+        return f"Hello have repayed{amount}"
+
+
+    
+
--- student.py
+++ student.py
@@ -1,9 +1,13 @@
 class Student:
     school="AkiraChix"
-    def __init__(self,name,age):
-        self.name=name 
+    def __init__(self,first_name,last_name,age):
+        self.first_name= first_name
+        self.last_name=last_name
         self.age=age
-    def speak(self):
-        return f"Hello class my name is{self.name}"
+    def great(self):
+        return f"Hello {self.first_name}welcome to {self.school}"
     def year_of_birth(self):
-        return f"She was born in {2021-self.age}"
+        return f"Hello you were born in{2021-self.age}"
+
+    def initials(self):
+        return f"Hello {self.first_name} {self.last_name} your initials are {self.first_name[0]}{self.last_name[0]} "
