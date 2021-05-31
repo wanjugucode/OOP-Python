3eca0a35759e444e387fa9439d8e642cee1cc35f
wanjugucode <karobiaanastasia.com>
2021-05-31 22:37:38 +0300

    code

--- bank.py
+++ bank.py
@@ -1,33 +1,72 @@
-
+from datetime import datetime
 class BankAcc:
     name="Equity"
-    def __init__(self,name,phone_number):
+
+
+    def __init__(self,name,phone_number,loan):
         self.name=name
         self.phone_number=phone_number
         self.balance=0
-        
+        self.loan=loan
+        self.statement=[]
+     
+
     def show_balance(self):
         return f"Hello{self.name}you balance is{self.balance}"
-    def deposit(self,amount):
-        self.balance+=amount
-        return self.show_balance()
 
     def withdraw(self,amount):
         if amount>self.balance:
             return f"you can't withdraw {amount}it is below minimum"
-          
+
         else:
              self.balance-=amount
              return self.show_balance()
-           
-       
-    def update_deposit(self,amount):
-        if amount>0:  
-            self.balance+=amount
-            return f"Hello your new balance is {self.show_balance}" 
+
+
+    def deposit(self,amount):
+        if amount<0:  
+            return f"you cannot deposit ksh:{amount} it is below minimum" 
         else:
-            return f"you cannot deposit{amount} it is beyound minimum"
-       
+            self.balance+=amount
+            now=datetime.now()
+            transaction={
+                "amount":50,
+                "time":now,
+                "narration":"you deposited"}
+            self.statement.append(transaction)
+            return self.statement
+    
+    def show_statement(self):
+        for transaction in self.statement:
+            amount=transaction["amount"]
+            narration=transaction["narration"]
+            time=transaction["time"]
+            date=time.strftime("%d/%m/%y")
+            print(f"{date}:{narration} {amount}")
+        return
+    def withdraw(self,amount):
+        if amount>self.balance:
+            return f"Your balance is {self.balance} and You cant't withdrwaw  {amount} it is below minimum"
+        else:
+            self.balance-=amount
+            now=datetime.now()
+            transaction={
+                "amount":70,
+                "time":now,
+                "narration":"You withdrew"
+            }
+            self.statement.append(transaction)
+            return self.showbalance()
+  
+    def show_statement(self):
+        for transaction in self.statement:
+            amount=transaction["amount"]
+            narration=transaction["narration"]
+            time=transaction["time"]
+            date=time.strftime("%d/%m/%y")
+            print(f"{date}:{narration} {amount}")
+        return
+
     def borrow(self,amount):
         self.loan=0
         self.loan+=amount
@@ -36,7 +75,6 @@ class BankAcc:
     def repay(self,amount):
         self.loan-=amount
         return f"Hello have repayed{amount}"
-
 
     
 
